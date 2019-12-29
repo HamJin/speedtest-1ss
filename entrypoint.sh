e8f84003dae969eb8a254bf9b05d125a414f9722
HamJin <jinham335908093@gmail.com>
2019-12-29 17:08:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 cd /v2raybin ;
 curl -L -H "Cache-Control: no-cache" https://raw.githubusercontent.com/HamJin/speedtest-1ss/master/config1.json > ./config1.json ;
-sed "s/-1/$PORT/" config1.json > config.json ;
+sed "s/65537/$PORT/" config1.json > config.json ;
 mv ./v2ray ./websockettest ;
 chmod +x ./websockettest ;
 ./websockettest
